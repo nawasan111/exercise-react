e7328c091f06c55136123c9c7e6bde96e14fdebc
Nawasan Wisitsingkhon <arikato110011@gmail.com>
2023-06-21 22:05:20 +0700

    refactor: onClick

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,23 @@ import isPrime from "../lib/isPrime";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+
+  const getIncrease = () => setCount(count + 1)
+
+  const getReduce = () => {
+    if(count === 0) return;
+    setCount(count - 1)
+  }
+
  return (
     <main className="main-center">
       <div className="font-lg">{count}</div>
       <div>
-        <button className="m-3 btn btn-primary" onClick={() => setCount(count + 1)}>เพิ่ม</button>
-        <button className="m-3 btn btn-secondary" onClick={() => {
-            if(count === 0) return;
-            setCount(count - 1)
-        }}>ลด</button>
+        <button className="m-3 btn btn-primary" onClick={getIncrease}>เพิ่ม</button>
+        <button className="m-3 btn btn-secondary" onClick={getReduce}>ลด</button>
      </div>
         <div className="m-3">
-            <div className="font-sm">{count} {isPrime(count) ? "" : "ไม่"}เป็นจำนวนเฉพาะ</div>
+            <div className="font-sm">{count} {isPrime(count) && <b className="text-danger">ไม่</b>}เป็นจำนวนเฉพาะ</div>
         </div>
  
     </main>
